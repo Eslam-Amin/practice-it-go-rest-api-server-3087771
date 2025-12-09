bfef581d34240e9c3140b1c60c437b971f329372
Eslam Amin <eslam.amin.610@gmail.com>
2025-12-09 02:54:09 +0200

    Created get All Orders function

--- src/backend/order.go
+++ src/backend/order.go
@@ -1,5 +1,7 @@
 package backend
 
+import "database/sql"
+
 type order struct {
 	ID           int         `json:"id"`
 	CustomerName string      `json:"customerName"`
@@ -12,4 +14,21 @@ type orderItem struct {
 	OrderID   int `json:"orderId"`
 	ProductID int `json:"productId"`
 	Quantity  int `json:"quantity"`
+}
+
+func getAllOrders(db *sql.DB) ([]order, error) {
+	rows, err := db.Query("Select * from orders")
+	if err != nil {
+		return nil, err
+	}
+	orders := []order{}
+	defer rows.Close()
+	for rows.Next() {
+		var ord order
+		if err := rows.Scan(&ord.ID, &ord.CustomerName, &ord.Total, &ord.Status); err != nil {
+			return nil, err
+		}
+		orders = append(orders, ord)
+	}
+	return orders, nil
 }
